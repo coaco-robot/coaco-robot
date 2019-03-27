c562033e4daa0fc1f92dc2bccb469908aa14c0e7
Salt_Factory <simon.vandevelde@hotmail.com>
2019-03-27 10:47:54 +0100

    Added __init__, pass img

--- src/marker_detector/src/detector.py
+++ src/marker_detector/src/detector.py
@@ -17,38 +17,12 @@ class Detector:
         print(ids)
         print(corners)
         rects = []
+        img_out = img_in
         for array in corners:
             rect = cv2.boundingRect(array)
             rects.append(rect)
+            cv2.rectangle(img_out, rect, (0, 255, 0), 3)
 
-        # aruco.drawDetectedMarkers(img_out, rejectedImgPoints,
-        #                          borderColor=(100, 0, 240))
-        return (ids, rects)
+        return ids, rects, img_out
 
-
-# image_name = "test"
-# image_ext = ".jpg"
-# 
-# AMOUNT_OF_IMG = 4
-# SIZE = 0.2
-# DICT = 16
-# cv2.namedWindow('image')
-# detector = Detector(16)
-# for number in range(1, 5):
-#     path = "../testfiles/{:s}{:d}{:s}".format(image_name, number, image_ext)
-#     img_in = cv2.imread(path)
-#     width = img_in.shape[0]
-#     height = img_in.shape[1]
-#     dim = (int(width*SIZE), int(height*SIZE))
-#     img_in = cv2.resize(img_in, dim)
-#     img_out = img_in
-# 
-#     ids, rects = detector.findMarkers(img_in)
-# 
-#     for rect in rects:
-#         cv2.rectangle(img_out, rect, (0, 255, 0), 1)
-# 
-#     cv2.imshow('image', img_out)
-#     cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
